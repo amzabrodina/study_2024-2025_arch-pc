59416aafc07c4b96dd972cb69b6c08f705f0719e
amzabrodina <zam050706@gmail.com>
2024-11-23 23:43:05 +0300

    Add files via upload

---
+++ labs/lab07/lab7-4.asm
@@ -0,0 +1,66 @@
+%include 'in_out.asm'
+SECTION .data
+prim1 DB 'x-5 ,x=>5' ,0
+prim2 DB 'ax, x<5',0
+X1 DB 'Введите значение Х:',0
+A1 DB 'Введите значение a:',0
+otv DB 'Ответ: ',0
+
+SECTION .bss
+X RESB 20
+A RESB 20
+F RESB 20
+SECTION .text
+GLOBAL _start
+_start:
+
+mov eax,prim1
+call sprintLF
+mov eax,prim2
+call sprintLF
+
+mov eax,X1
+call sprint
+
+mov ecx,X
+mov edx,10
+call sread
+
+mov eax,X
+call atoi
+mov [X],eax
+
+mov eax,A1
+call sprint
+
+mov ecx,A
+mov edx,10
+call sread
+
+mov eax,A
+call atoi
+mov [A],eax
+
+mov ecx,[X]
+mov [F],ecx
+
+cmp ecx,[A]
+jl check_or
+mov ecx,8
+mov [F],ecx
+jmp fin
+
+check_or:
+mov eax,[A]
+mov ebx,2
+mul ebx
+mov ecx, [X]
+sub eax,ecx
+mov [F],eax
+
+fin:
+mov eax,otv
+call sprint
+mov eax,[F]
+call iprintLF
+call quit
